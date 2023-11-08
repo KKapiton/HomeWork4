57ae54e352b77cc526abe5d4df1245b2b53e67eb
Kirill <fef1911@yandex.ru>
2023-11-08 21:30:56 +0300

    Initial commit

---
+++ src/main/java/Work4/Work4_1.java
@@ -0,0 +1,17 @@
+package Work4_1;
+
+public static void main(String[]args){
+        int number = 10;
+        String convertNumber = Convertnumber.
+        }
+public class Work4_1 {
+    public static String convertNumber (int number) {
+        String result;
+        {
+            result = (number > 0) ? (number % 7 < 4) ? ("positive-m") : ("positive-p")
+                    : (number == 0) ? ("zero")
+                    : (number % 7 > -4) ? ("negative-n") : ("negative-p");
+        }
+        return result;
+    }
+}
---
+++ src/main/java/Work4_3/Work4_3.java
@@ -0,0 +1,8 @@
+package Work4_2;
+
+public class Work4_2 {
+    public static void main(String[] args) {
+        int [] input = { 1,2,3,1,8,9 };
+
+    }
+}
